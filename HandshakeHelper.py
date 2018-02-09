366ab274685578ac033dc2c90d4597e9948e84ca
Dave Debreceni <davidjr@debreceni.net>
2018-02-09 17:04:19 -0500

    Created General domain reader function to work on any file

--- HandshakeHelper.py
+++ HandshakeHelper.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 import sys
-import csv
+import re
 import json
 
 
 class HandshakeHelper(object):
     def __init__(self):
+        self.pattern = r'(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}'
         self.json_data = []
 
-    def domain_reader(self, file, dialect='excel'):
+    def domain_reader(self, file):
+        """
+        Extracts all domains from given file and wraps them as a json object.
+        :param file: File of any type containing any amount of domains.
+        :return: None
+        """
         try:
-            if file.endswith('.txt'):
-                with open(file) as txt_file:
-                    self.json_data = [x[:-2] for x in txt_file]
-            else:
-                with open(file, newline='') as csv_file:
-                    csv_reader = csv.reader(csv_file, dialect)
-                    self.json_data = [x for x in csv_reader]
-                    print(self.json_data)
-                    #print(self.json_data)
+            with open(file, 'r') as domain_file:
+                regex_match = re.findall(self.pattern, domain_file.read())
+                self.json_data = json.dumps(regex_match)
+                print(self.json_data)
         except FileNotFoundError:
             print(file+' was not found.')
             sys.exit()
